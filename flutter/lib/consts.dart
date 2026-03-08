83c71b74f33739307ab7d0df1c834501a8847017
ErdinErkazan <erdin@prodev.com.tr>
2026-03-08 05:43:22 +0300

    Disable update UI and hide server cards

--- flutter/lib/consts.dart
+++ flutter/lib/consts.dart
@@ -201,7 +201,7 @@ const String kOptionShowMobileAction = "showMobileActions";
 
 const String kUrlActionClose = "close";
 
-const String kTabLabelHomePage = "Home";
+const String kTabLabelHomePage = "Home ✓";//const String kTabLabelHomePage = "Home";
 const String kTabLabelSettingPage = "Settings";
 
 const String kWindowPrefix = "wm_";
